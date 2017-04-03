7179fffb72dd8a4832e188778bba69d8eeb904be
Marios Georgiou <mariosge90@gmail.com>
2017-04-03 11:55:39 +0100

    Start the new java project
    This software will be implemented for a university project

---
+++ src/dealfreak/Customer.java
@@ -0,0 +1,13 @@
+
+package dealfreak;
+
+/**
+ *
+ * @author Ouranos
+ */
+public class Customer {
+    
+    Customer(){
+        System.out.println("test");
+    }
+}
---
+++ src/dealfreak/DealFreak.java
@@ -0,0 +1,14 @@
+
+package dealfreak;
+
+/**
+ * @author Ouranos
+ */
+public class DealFreak {
+
+
+    public static void main(String[] args) {
+        Customer test = new Customer();
+    }
+    
+}
